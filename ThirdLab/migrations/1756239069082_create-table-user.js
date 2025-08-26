e2a7cccf1fba8d88b6c65e0b8eb06ec7a7d462cf
wayanpratama38 <wayanpratama38@gmail.com>
2025-08-27 03:19:00 +0700

    feat: :art: create users table

--- ThirdLab/migrations/1756239069082_create-table-user.js
+++ ThirdLab/migrations/1756239069082_create-table-user.js
@@ -8,7 +8,27 @@ export const shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+export const up = (pgm) => {
+    pgm.createTable('users',{
+        id : {
+            type : "VARCHAR(50)",
+            primaryKey :true
+        },
+        username : {
+            type : "VARCHAR(50)",
+            unique : true,
+            notNull : true
+        },
+        password : {
+            type : "TEXT",
+            notNull : true
+        },
+        fullname : {
+            type : "TEXT",
+            notNull : true
+        }
+    })
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
