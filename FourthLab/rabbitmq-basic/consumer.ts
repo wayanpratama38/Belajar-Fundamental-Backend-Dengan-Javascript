9f05d2dd5275c61ec7d67502d8e6b3bbb8a1498a
wayanpratama38 <wayanpratama38@gmail.com>
2025-09-13 19:44:33 +0700

    feat: :art: create basic rabbitmq project

---
+++ FourthLab/rabbitmq-basic/consumer.ts
@@ -0,0 +1,19 @@
+import * as amqp from 'amqplib';
+
+const init = async() =>{
+    const connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queue = 'dicoding';
+
+    await channel.assertQueue(queue,{
+        durable : true
+    });
+
+    channel.consume(queue,(message) => {
+        console.log(message);
+        console.log(`Message dari ${queue} : ${message?.content.toString()}`)
+    }, {noAck : true});
+}
+
+init();
