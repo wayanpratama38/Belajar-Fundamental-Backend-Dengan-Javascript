22299a9a60c8a7e6aa1338ddd257a96e19cfccd8
wayanpratama38 <wayanpratama38@gmail.com>
2026-01-17 20:39:19 +0700

    feat: :tada: initialize first submission using express as a framework

---
+++ FirstSubmission-Express/src/exceptions/ClientError.js
@@ -0,0 +1,8 @@
+
+export default class ClientError extends Error {
+  constructor(message, statusCode = 400) {
+    super(message);
+    this.name = "ClientError";
+    this.statusCode = statusCode;
+  }
+}
---
+++ FirstSubmission-Express/src/server.js
@@ -0,0 +1,12 @@
+import server from "./server/index.js";
+
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3000;
+
+server.get("/", (req, res) => {
+  return res.json({ status: "SUCCESS" });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
